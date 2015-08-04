81acfdc48289989d4cfe0eba81e3b5811efdac1c
Tyler Austin Deem & Deron Johnson <student@epicodus.com>
2015-08-04 12:28:25 -0700

    Add images and HTML in app file.

--- app/app.php
+++ app/app.php
@@ -5,7 +5,37 @@
     $app = new Silex\Application();
 
     $app->get("/", function(){
-        return "Home";
+        $first_cd = new CD("Hummingbird", "Local Natives", "img/hummingbird.jpg");
+        $second_cd = new CD("One Big Particular Loop", "Polyenso", "img/polyenso.jpg");
+        $third_cd = new CD("Nevermind", "Nirvana", "img/nirvana.jpg");
+        $fourth_cd = new CD("Clouds Make Sounds", "Clouds Make Sounds", "img/clouds.jpg", 19.99);
+        $cds = array($first_cd, $second_cd, $third_cd, $fourth_cd);
+
+        $output = "";
+        foreach ($cds as $album) {
+            $output = $output . "
+            <!DOCTYPE html>
+            <html>
+            <head>
+              <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css'>
+            </head>
+            <body>
+                <div class='row'>
+                  <div class='col-md-6'>
+                      <img src=" . $album->getCoverArt() . ">
+                  </div>
+                  <div class='col-md-6'>
+                      <p>" . $album->getTitle() . "</p>
+                      <p>By: " . $album->getArtist() . "</p>
+                      <p>$" . $album->getPrice() . "</p>
+                  </div>
+                </div>
+            </body>
+            </html>
+            ";
+        }
+
+        return $output;
     });
 
     return $app;
